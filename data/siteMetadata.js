0baec8d704a74b4af673399036506165046f67c2
Shivasurya <s5sankarapandian@uwaterloo.ca>
2023-04-19 22:07:29 -0400

    added blog name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'DEVAA Security Blog',
+  author: 'Shiva',
+  headerTitle: 'DEVAA Security Bytes',
+  description: 'Fortify your apps with DEVAA Security',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://devaa-security-blog.netlify.app',
+  siteRepo: 'https://github.com/devaa-security/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
